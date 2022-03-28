d061c64ea07b96995fb2842c76063ce90f284fe1
Santhosh John <santhoshjohn78@gmail.com>
2022-03-28 15:37:22 -0700

    Update HbaseClientSample.scala

--- src/main/scala/HbaseClientSample.scala
+++ src/main/scala/HbaseClientSample.scala
@@ -1,6 +1,8 @@
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.ConnectionFactory
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.client.{ConnectionFactory, Get, Put, Scan}
+import org.apache.hadoop.hbase.util.Bytes
+
 
 object HbaseClientSample {
 
@@ -21,12 +23,39 @@ object HbaseClientSample {
     val admin = connection.getAdmin()
 
     val listtables=admin.listTables()
+
     println("******************************************")
 
     listtables.foreach(println)
 
     println("__________________________________________")
 
+    val table = connection.getTable(TableName.valueOf("users"))
+
+    val key = new Get(Bytes.toBytes(1))
+
+    val row = table.get(key);
+
+    println(row);
+    println("_____________before adding rows_____________________________")
+
+    val newrow = new Put(Bytes.toBytes(2))
+
+    newrow.addColumn(Bytes.toBytes("cf"),Bytes.toBytes("Name"),Bytes.toBytes("Santhosh"))
+    newrow.addColumn(Bytes.toBytes("cf"),Bytes.toBytes("age"),Bytes.toBytes("88"))
+
+    table.put(newrow)
+    println("_____________after adding rows_____________________________")
+    val scan = new Scan()
+    scan.addColumn(Bytes.toBytes("cf"),Bytes.toBytes("Name"))
+    scan.addColumn(Bytes.toBytes("cf"),Bytes.toBytes("age"))
+    val scaniterator = table.getScanner(scan).iterator()
+
+    scaniterator.forEachRemaining({x =>
+      println(Bytes.toString(x.getValue(Bytes.toBytes("cf"),Bytes.toBytes("Name"))))
+    })
+
+    connection.close()
     println("End....")
   }
 
